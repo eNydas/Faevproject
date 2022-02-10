9ac511b8280474ef5c6ff5a6af12ebd8a0689fbc
eNydas <danyvargase@gmail.com>
2022-02-09 20:11:49 -0500

    Diseño de la papgina
    Actualizacion 1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,6 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("abajo", window.scrollY>50)
 })
+
+//HISTORIA SCROLL REVEAL//
+
