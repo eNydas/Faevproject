976c593969bf7df31f4afd9abf97ebe8f991a3a4
eNydas <danyvargase@gmail.com>
2022-04-08 16:27:45 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,5 +105,25 @@ function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
 
+/*===== Gallery =====*/
 
+'use strict'
+const enlace = document.querySelectorAll('.a')
+const lightbox = document.querySelector('.gallery__overlay')
+const grande = document.querySelector('.grande')
+const cerrar = document.querySelector('.close__lightbox')
 
+enlace.forEach(( cadaEnlace , i )=>{
+    enlace[i].addEventListener('click', ( e )=>{
+        e.preventDefault()
+        let img = cadaEnlace.querySelector('.img').src
+        console.log( img ) 
+        lightbox.classList.add('activo')
+        grande.setAttribute('src', img)
+
+    })
+})
+
+cerrar.addEventListener('click', ()=>{
+    lightbox.classList.remove('activo')
+})
