c9bcc43fda6bbe04035cfbc929c8665658557c86
eNydas <danyvargase@gmail.com>
2022-04-13 18:33:09 -0500

    update metaetiquetas

--- sw.js
+++ sw.js
@@ -48,3 +48,20 @@ self.addEventListener('activate', e => {
   )
 })
 
+//ESTA PARTE DEL CODIGO LA ELIMINE PORQUE NO SE COMO CONFIGURARLA.
+/*cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      })
+  )
+})
+*/
