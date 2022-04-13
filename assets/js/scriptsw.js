91ba920c85177123ba50b74cb1eba4679a03fb2c
eNydas <danyvargase@gmail.com>
2022-04-13 16:49:10 -0500

    PWA dev

---
+++ assets/js/scriptsw.js
@@ -0,0 +1,6 @@
+//pwa
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../../sw.js')
+      .then(reg => console.log('Registro de SW exitoso', reg))
+      .catch(err => console.warn('Error al tratar de registrar el sw', err))
+  }
