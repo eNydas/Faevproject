fa935c479d299cd0a73d77ace3815665f1c14f9c
eNydas <danyvargase@gmail.com>
2022-04-14 14:19:59 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -65,3 +65,11 @@ self.addEventListener('fetch', e => {
   )
 })
 */
+
+// fetch
+
+self.addEventListener('fetch', e => {
+  console.log('Service Worker: Fetching');
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  
+});
